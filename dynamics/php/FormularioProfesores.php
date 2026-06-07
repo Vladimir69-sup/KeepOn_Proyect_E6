f8978301180aceb8997f7679203771f7e22d5e77
Ruben Isaac Gonzalez Peña <ruben.200831gmail.com>
2026-06-07 13:43:42 -0600

    feat: encabezado de del buscador de profesores

---
+++ dynamics/php/FormularioProfesores.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario Profesores</title>
+    <link rel="stylesheet" href="../../statics/css/style.css">
+</head>
+<header>
+    <div class="contenedor-encabezado">
+        <img src="../../statics/media/img/imagenUNAM.png" height="80px">
+        <img src="../../statics/media/img/imagenENP.png" height="80px">
+        <img src="../../statics/media/img/imagenETE.png" height="80px">
+        <img src="../../statics/media/img/imagenComputacion.png" height="80px">
+        <input class="buscador" type="text" placeholder="Busca algo aquí">
+        <button class="buscador" name="buscador" type="submit"><img style="border-radius: 100px;" src="../../statics/media/img/imagenLupa.png" height="50px"></button>
+
+    </div>
+</header>
+<body>
+    
+</body>
+</html>
+
+<?php
+
+?>
