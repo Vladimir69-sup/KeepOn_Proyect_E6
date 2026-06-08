aeda702a59f97fba423c5ba1d126ac36c92cba9e
Evelin Guadalupe Martinez Sevilla <martinezsevillaevelinguadalupe@gmail.com>
2026-06-08 07:32:19 -0600

    feat:se agrego estructura evelin

---
+++ dynamics/php/InicioDeSesionAdministrador.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device_width, initial_scaled=1.0">
+    <meta name="author" content="Evelin Guadalupe Martinez Sevilla">
+    <meta name="description" content="Inicio de sesion del alumno">
+    <title>Inicio de Sesion</title>
+    <link rel="stylesheet" href="../../statics/css/InicioDeSesionAlumno.css">
+	</head>
+<body>
+    <h2>Inicio de Sesion</h2>
+    <img src="../../../img/monkey-admin.png" width="150px">
+    <form>
+        <label for="usuario">Usuario:</label><br>
+        <input type="text" id="username" name="username" placeholder="no. de matricula"><br>
+        <label for="contraseña">Contraseña:</label><br>
+        <input type="password" id="pwd" name="pwd" placeholder="dd/mm/aaaa"><br>
+        <input type="submit" value="Inicia Sesion">
+    </form>
+</body>
+</html>
