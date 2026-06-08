bf359f71a9d7784ad50b31b0e45279a02616257e
Gabriela Abigail Escamilla Flores <324102374@alumno.enp.unam.mx>
2026-06-08 14:38:20 -0600

    feat: añadir vista de perfil del alumno

---
+++ dynamics/php/VistaPerfilAlumno.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VistaPerfilAlumno</title>
+    <link rel="stylesheet" href="../../statics/css/VistaPerfilAlumno.css">
+</head>
+<body>
+    <div class="contenedor-apartados">
+        <div class="datos-alumno">
+            <img class="foto-perfil" src="../../statics/media/img/FotoPerfil.png" alt="Foto de Perfil">
+            <div>
+                <p>Nombre del Alumno (Tú)</p>
+                <p>Número de Cuenta</p>
+                <p>Fecha de Nacimiento</p>
+                <p>Grupo al que pertenece</p> 
+            </div>
+            
+            <button class="editar-perfil">Editar foto de Perfil</button>
+        </div>
+        <div class="dos-secciones">
+            <div class="respuestas-form">
+                <p>Condiciones de Estudio</p>
+                <p>El cuestionario aún no ha sido resuelto</p>
+            </div>
+            <div class="inferior-derecho">
+                <button id="formulario-condiciones">Formulario Condiciones de Estudio</button>
+                <p>Notas de tu profesor: </p>
+                <div class="notas-profesor">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fu</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
