276be3b58ac178e0ec2dd37bf89ce45e922abed2
Gabriela Abigail Escamilla Flores <324102374@alumno.enp.unam.mx>
2026-06-09 13:51:30 -0600

    feat(botón): Hacer que se oculte y muestre el label botón guardar

--- dynamics/php/VistaPerfilAlumno.php
+++ dynamics/php/VistaPerfilAlumno.php
@@ -1,3 +1,17 @@
+<?php
+    session_start();
+    $rutaFoto = "../../statics/media/img/";
+    $nombreFoto = "FotoUsuario.png";
+    $fotoAlumno = $rutaFoto . $nombreFoto;
+
+    if(isset($_POST['guardar-foto'])){
+        move_uploaded_file($_FILES['foto-perfil-alumno']['tmp_name'], $fotoAlumno);
+    }
+    if (!file_exists($fotoAlumno)) {
+    $fotoAlumno = $rutaFoto . "FotoPerfil.png";
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +23,7 @@
 <body>
     <div class="contenedor-apartados">
         <div class="datos-alumno">
-            <img class="foto-perfil" src="../../statics/media/img/FotoPerfil.png" alt="Foto de Perfil">
+            <img class="foto-perfil" src="<?php echo $fotoAlumno;?>" alt="Foto de Perfil">
             <div>
                 <p>Nombre del Alumno (Tú)</p>
                 <p>Número de Cuenta</p>
@@ -17,11 +31,16 @@
                 <p>Grupo al que pertenece</p> 
             </div>
             
-            <button class="editar-perfil">Editar foto de Perfil</button>
+            <form class="mostrar-abajo" method="POST" enctype="multipart/form-data">
+                <input type="file" name="foto-perfil-alumno" id="inpt-ftalumno" accept="image/png, image/jpeg" style="display: none;" required>
+                <label for="inpt-ftalumno" class="editar-perfil">Editar foto de Perfil</label>
+                <button type="submit" name="guardar-foto" class="guardar-foto"> Guardar</button>
+            </form>
+
         </div>
         <div class="dos-secciones">
             <div class="respuestas-form">
-                <p>Condiciones de Estudio</p>
+                <h1>Condiciones de Estudio</h1>
                 <p>El cuestionario aún no ha sido resuelto</p>
             </div>
             <div class="inferior-derecho">
