86d7dfdb16233b4643cba91646c4accde1096053
Vladimir Ortiz Ochoa <vladimirortizochoa62@gmail.com>
2026-06-09 17:02:27 -0600

    feat(admin): se añade vista inicial del administrador

---
+++ dynamics/AdminVistaIni.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!--Meta datos-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./../statics/css/AdminVista1.css">
+    <title>Página web KEEP ON</title>
+</head>
+<body>
+
+<main>
+    <!--barra de navegación-->
+    <div id="caja-principal">
+        <div class="cajas" id="c-1">
+            <a href="./AdminVistaListado.php">
+                <img class="imgs" src="./../statics/media/img/lupa.png" alt="Imagen de una lupa">
+            </a>
+            <div class="encabezado-op">
+            <h2>Alumnos y profesorado</h2>
+            </div>
+        </div>
+        <div class="cajas" id="c-2">
+            <a href="./AdminVistaListado.php">
+                    <img class="imgs" src="./../statics/media/img/llaves.png" alt="Imagen de llaves">
+            </a>
+            <div class="encabezado-op">
+                <h2>Llaves administrador</h2>
+            </div>
+        </div>
+    </div>
+</main>
+</body>
+</html>
