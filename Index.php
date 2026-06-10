32ca24f13f44b4b2194ba0441eba648b194e72c1
Evelin Guadalupe Martinez Sevilla <martinezsevillaevelinguadalupe@gmail.com>
2026-06-09 23:06:58 -0600

    feat:añade la pagina de inicio

---
+++ Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device_width, initial_scaled=1.0">
+    <meta name="author" content="Evelin Guadalupe Martinez Sevilla">
+    <meta name="description" content="Pagina de inicio">
+    <link rel="stylesheet" href="index.css">
+    <title>Home</title>
+    </head>
+<body>
+    <div class="botoncitos">
+        <div class="botones-de-arriba">
+            <button class="boti" id="alumno">Alumnado
+            <img src="../img/puma-uno-Photoroom.png" alt="Puma1" class="puma-al">
+            </button>
+            <button class="boti-dos" id="profe">Profesorado
+            <img src="../img/puma-dos-removebg-preview.png" alt="Puma2" class="puma-pro">
+            </button>
+        </div>
+        <button class="boti-tres" id="admin">Administracion
+        <img src="../img/puma-tres-removebg-preview.png" alt="Puma3" class="puma-ad">
+        </button>
+    </div>
+</body>
+</html>
