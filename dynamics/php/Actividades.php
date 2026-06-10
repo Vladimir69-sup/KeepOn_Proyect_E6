713242032086e69e5d4ec6a955620f8a0857eb5a
karla-ksksks <ksjkskjskkjkkjjljkjkjjjsjsjjsj@gmail.com>
2026-06-09 20:19:58 -0600

    feat(actividades) : agrega logica a actividades.php

--- dynamics/php/Actividades.php
+++ dynamics/php/Actividades.php
@@ -9,26 +9,198 @@
 <body>
     <h1>ACTIVIDADES</h1>
     <br>
-    <h2>Módulo 1 <button id="eliminar-modulo-1"><img src="../../statics/media/img/imagenBorrar.png" class ="boton-borrar"></button></h2>
+    <h2>Módulo 1 <button class="eliminar-modulo"><img src="../../statics/media/img/imagenBorrar.png" class ="boton-borrar"></button></h2>
     <div class="contenedor-modulo">
         <a href="AgregarActividad.php"><div class="crear-tarea">+<br> Agregar una Nueva Actividad</div></a>
 
-        <a href="EditarActividad.php"><div class="editar-tarea">
-            <p>Título</p>
-            <p>Estado: Publicado</p>
-            <p>Fecha de Entrega</p>
-            <p></p>
-            <p>(datos generales de la actividad ya creada)</p>
-        </div></a>
+        <?php
+            include './conexion.php';
 
-        <a href="EditarActividad.php"><div class="editar-tarea">
-            <p>Título</p>
-            <p>Estado: Publicado</p>
-            <p>Fecha de Entrega</p>
-            <p></p>
-            <p>(datos generales de la actividad ya creada)</p>
-        </div></a>
+            $idMaestroActual=1;
+            $consultaGrupo="SELECT idGrupo FROM grupo WHERE idMaestro=$idMaestroActual";
+            $query=mysqli_query($conexion,$consultaGrupo);//esta hace la ncosnulta por nostros 
+            $query_info=mysqli_fetch_assoc($query); //obtenemos registro
+            $idGrupo=$query_info["idGrupo"];//con esto ya tiene como tal el valor e idGrupo
+
+            $consulta="SELECT * FROM actividad WHERE idGrupo=$idGrupo AND modulo=1";
+            $queri=mysqli_query($conexion,$consulta);//esta hace la ncosnulta por nostros 
+            
+            while( $query_info_actividad=mysqli_fetch_assoc($queri))
+            {
+               // <p>(datos generales de la actividad ya creada)</p>//descripcion??
+                $id = $query_info_actividad["idActividad"];
+                $titulo=$query_info_actividad["titulo"];
+                $modulo=$query_info_actividad["modulo"];//almacenamos en variables los valores el contenido del arreglo asociativo
+                $fecha=$query_info_actividad["fecha"];//para que sea mas facil de entender visualmente
+                $hora=$query_info_actividad["hora"];
+
+                echo "<a href='EditarActividad.php?id=$id'><div class='editar-tarea'>";
+                echo "<p>$titulo</p>";
+                echo "<p>modulo:$modulo</p>";
+                echo "<p>Fecha de Entrega:$fecha</p>";
+                echo "<p>Hora de Entrega: $hora</p>";
+                //<p></p>
+                echo "</div></a> ";
+
+            }
+                   
+        ?>
+
     </div>
-    <button id="crear-modulo">+Agrega Modulo</button> 
-</body>
+
+    <h2>Módulo 2 <button class="eliminar-modulo"><img src="../../statics/media/img/imagenBorrar.png" class ="boton-borrar"></button></h2>
+    <div class="contenedor-modulo">
+        <a href="AgregarActividad.php"><div class="crear-tarea">+<br> Agregar una Nueva Actividad</div></a>
+
+        <?php
+
+            $idMaestroActual=1;
+            $consultaGrupo="SELECT idGrupo FROM grupo WHERE idMaestro=$idMaestroActual";
+            $query=mysqli_query($conexion,$consultaGrupo);//esta hace la ncosnulta por nostros 
+            $query_info=mysqli_fetch_assoc($query); //obtenemos registro
+            $idGrupo=$query_info["idGrupo"];//con esto ya tiene como tal el valor e idGrupo
+
+            $consulta="SELECT * FROM actividad WHERE idGrupo=$idGrupo AND modulo=2";
+            $queri=mysqli_query($conexion,$consulta);//esta hace la ncosnulta por nostros 
+            
+            while( $query_info_actividad=mysqli_fetch_assoc($queri))
+            {
+               // <p>(datos generales de la actividad ya creada)</p>//descripcion??
+                $id = $query_info_actividad["idActividad"];
+                $titulo=$query_info_actividad["titulo"];
+                $modulo=$query_info_actividad["modulo"];//almacenamos en variables los valores el contenido del arreglo asociativo
+                $fecha=$query_info_actividad["fecha"];//para que sea mas facil de entender visualmente
+                $hora=$query_info_actividad["hora"];
+
+                echo "<a href='EditarActividad.php?id=$id'><div class='editar-tarea'>";
+                echo "<p>$titulo</p>";
+                echo "<p>modulo:$modulo</p>";
+                echo "<p>Fecha de Entrega:$fecha</p>";
+                echo "<p>Hora de Entrega: $hora</p>";
+                //<p></p>
+                echo "</div></a> ";
+
+            }
+                   
+        ?>
+
+    </div>
+
+    <h2>Módulo 3 <button class="eliminar-modulo"><img src="../../statics/media/img/imagenBorrar.png" class ="boton-borrar"></button></h2>
+    <div class="contenedor-modulo">
+        <a href="AgregarActividad.php"><div class="crear-tarea">+<br> Agregar una Nueva Actividad</div></a>
+
+        <?php
+
+            $idMaestroActual=1;
+            $consultaGrupo="SELECT idGrupo FROM grupo WHERE idMaestro=$idMaestroActual";
+            $query=mysqli_query($conexion,$consultaGrupo);//esta hace la ncosnulta por nostros 
+            $query_info=mysqli_fetch_assoc($query); //obtenemos registro
+            $idGrupo=$query_info["idGrupo"];//con esto ya tiene como tal el valor e idGrupo
+
+            $consulta="SELECT * FROM actividad WHERE idGrupo=$idGrupo AND modulo=3";
+            $queri=mysqli_query($conexion,$consulta);//esta hace la ncosnulta por nostros 
+            
+            while( $query_info_actividad=mysqli_fetch_assoc($queri))
+            {
+               // <p>(datos generales de la actividad ya creada)</p>//descripcion??
+                $id = $query_info_actividad["idActividad"];
+                $titulo=$query_info_actividad["titulo"];
+                $modulo=$query_info_actividad["modulo"];//almacenamos en variables los valores el contenido del arreglo asociativo
+                $fecha=$query_info_actividad["fecha"];//para que sea mas facil de entender visualmente
+                $hora=$query_info_actividad["hora"];
+
+                echo "<a href='EditarActividad.php?id=$id'><div class='editar-tarea'>";
+                echo "<p>$titulo</p>";
+                echo "<p>modulo:$modulo</p>";
+                echo "<p>Fecha de Entrega:$fecha</p>";
+                echo "<p>Hora de Entrega: $hora</p>";
+                //<p></p>
+                echo "</div></a> ";
+
+            }
+                   
+        ?>
+
+    </div>
+
+    <h2>Módulo 4 <button class="eliminar-modulo"><img src="../../statics/media/img/imagenBorrar.png" class ="boton-borrar"></button></h2>
+    <div class="contenedor-modulo">
+        <a href="AgregarActividad.php"><div class="crear-tarea">+<br> Agregar una Nueva Actividad</div></a>
+
+        <?php
+
+            $idMaestroActual=1;
+            $consultaGrupo="SELECT idGrupo FROM grupo WHERE idMaestro=$idMaestroActual";
+            $query=mysqli_query($conexion,$consultaGrupo);//esta hace la ncosnulta por nostros 
+            $query_info=mysqli_fetch_assoc($query); //obtenemos registro
+            $idGrupo=$query_info["idGrupo"];//con esto ya tiene como tal el valor e idGrupo
+
+            $consulta="SELECT * FROM actividad WHERE idGrupo=$idGrupo AND modulo=4";
+            $queri=mysqli_query($conexion,$consulta);//esta hace la ncosnulta por nostros 
+            
+            while( $query_info_actividad=mysqli_fetch_assoc($queri))
+            {
+               // <p>(datos generales de la actividad ya creada)</p>//descripcion??
+                $id = $query_info_actividad["idActividad"];
+                $titulo=$query_info_actividad["titulo"];
+                $modulo=$query_info_actividad["modulo"];//almacenamos en variables los valores el contenido del arreglo asociativo
+                $fecha=$query_info_actividad["fecha"];//para que sea mas facil de entender visualmente
+                $hora=$query_info_actividad["hora"];
+
+                echo "<a href='EditarActividad.php?id=$id'><div class='editar-tarea'>";
+                echo "<p>$titulo</p>";
+                echo "<p>modulo:$modulo</p>";
+                echo "<p>Fecha de Entrega:$fecha</p>";
+                echo "<p>Hora de Entrega: $hora</p>";
+                //<p></p>
+                echo "</div></a> ";
+
+            }
+                   
+        ?>
+
+    </div>
+
+    <h2>Módulo 5 <button class="eliminar-modulo"><img src="../../statics/media/img/imagenBorrar.png" class ="boton-borrar"></button></h2>
+    <div class="contenedor-modulo">
+        <a href="AgregarActividad.php"><div class="crear-tarea">+<br> Agregar una Nueva Actividad</div></a>
+
+        <?php
+
+            $idMaestroActual=1;
+            $consultaGrupo="SELECT idGrupo FROM grupo WHERE idMaestro=$idMaestroActual";
+            $query=mysqli_query($conexion,$consultaGrupo);//esta hace la ncosnulta por nostros 
+            $query_info=mysqli_fetch_assoc($query); //obtenemos registro
+            $idGrupo=$query_info["idGrupo"];//con esto ya tiene como tal el valor e idGrupo
+
+            $consulta="SELECT * FROM actividad WHERE idGrupo=$idGrupo AND modulo=5";
+            $queri=mysqli_query($conexion,$consulta);//esta hace la ncosnulta por nostros 
+            
+            while( $query_info_actividad=mysqli_fetch_assoc($queri))
+            {
+               // <p>(datos generales de la actividad ya creada)</p>//descripcion??
+                $id = $query_info_actividad["idActividad"];
+                $titulo=$query_info_actividad["titulo"];
+                $modulo=$query_info_actividad["modulo"];//almacenamos en variables los valores el contenido del arreglo asociativo
+                $fecha=$query_info_actividad["fecha"];//para que sea mas facil de entender visualmente
+                $hora=$query_info_actividad["hora"];
+
+                echo "<a href='EditarActividad.php?id=$id'><div class='editar-tarea'>";
+                echo "<p>$titulo</p>";
+                echo "<p>modulo:$modulo</p>";
+                echo "<p>Fecha de Entrega:$fecha</p>";
+                echo "<p>Hora de Entrega: $hora</p>";
+                //<p></p>
+                echo "</div></a> ";
+
+            }
+                   
+        ?>
+
+    </div>
+    
+    
+
+    </body>
 </html>
