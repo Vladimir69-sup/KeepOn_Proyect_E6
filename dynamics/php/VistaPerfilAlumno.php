70d7e906ceea6d6f669442dc4d99aacec6a996fe
Gabriela Abigail Escamilla Flores <324102374@alumno.enp.unam.mx>
2026-06-10 23:02:26 -0600

    feat(form): añadir que muestre los datos del formulario NOTA:aún no funciona

--- dynamics/php/VistaPerfilAlumno.php
+++ dynamics/php/VistaPerfilAlumno.php
@@ -10,6 +10,35 @@
     if (!file_exists($fotoAlumno)) {
     $fotoAlumno = $rutaFoto . "FotoPerfil.png";
     }
+
+    //CONECTAR A LA BASE DE DATOS   
+    const DBHOST = "localhost";
+    const DBUSER = "root";
+    const PASSWORD = "";
+    const DB = "keep_on_db";
+
+    $conexion = mysqli_connect(DBHOST, DBUSER, PASSWORD, DB);
+
+    //$id_formulario = $_GET['id_formulario']; 
+    $id_formulario = isset($_REQUEST['id_formulario']) ? $_REQUEST['id_formulario'] : 1;
+    //este es solo de prueba
+    $idUsuario = 1;
+
+    //Coonsulta el estado del formulario
+    $consultaEstado = "SELECT enviado FROM formulario WHERE idFormulario=$id_formulario";
+    $resultadoEstado = mysqli_query($conexion, $consultaEstado);
+    $paqEstado = $resultadoEstado->fetch_array();
+    $formResuelto = $paqEstado['enviado'];
+    //Consultas de nuevo
+    $consultaPreguntas = "SELECT pregunta, idPregunta, idTipoPregunta FROM pregunta WHERE idFormulario=$id_formulario";
+    $preguntas = mysqli_query($conexion, $consultaPreguntas);
+    $totalPreguntas = mysqli_num_rows($preguntas); //cuenta el total de preguntas que hay para poder recorrer esa cantidad en el for
+
+    $tipos = [
+        1 => 'radio',
+        2 => 'checkbox',
+        3 => 'textarea'
+    ]; 
 ?>
 
 <!DOCTYPE html>
@@ -31,7 +60,7 @@
                 <p>Grupo al que pertenece</p> 
             </div>
             
-            <form class="mostrar-abajo" method="POST" enctype="multipart/form-data">
+            <form class="mostrar-abajo" action="" method="POST" enctype="multipart/form-data">
                 <input type="file" name="foto-perfil-alumno" id="inpt-ftalumno" accept="image/png, image/jpeg" style="display: none;" required>
                 <label for="inpt-ftalumno" class="editar-perfil">Editar foto de Perfil</label>
                 <button type="submit" name="guardar-foto" class="guardar-foto"> Guardar</button>
@@ -41,10 +70,66 @@
         <div class="dos-secciones">
             <div class="respuestas-form">
                 <h1>Condiciones de Estudio</h1>
-                <p>El cuestionario aún no ha sido resuelto</p>
+                <?php
+                if($formResuelto == true){
+                    for($num = 0; $num<$totalPreguntas; $num++){
+                    //
+                        $paqPreguntas = $preguntas->fetch_array();
+                        $textPreguntas = $paqPreguntas['pregunta'];
+                        $textTipoPregunta = $paqPreguntas['idTipoPregunta']; 
+                        $idPregunta = $paqPreguntas['idPregunta'];
+                        $tipoInput = $tipos[$textTipoPregunta];
+
+                        $nombreInput = "respuesta_" . $idPregunta; //para saber el name="" de cada input
+                        //Si se acaba de enviar el formulario
+                        if(isset($POST['enviar-form'])){//el botón del otro formulario, es su name
+                            if(isset($_POST[$nombreInput])){
+                                if($tipoInput == 'checkbox'){
+                                    $seleccionesAlumno = "";
+                                    foreach($_POST[$nombreInput] as $opcionSelec){
+                                        $seleccionesAlumno = $seleccionesAlumno . $opcionSelec; //Se pone para que las guarde y no solamente deje la última
+
+                                        $sqlInsert="INSERT INTO respuestaUsuario (textoRespuesta, idUsuario, idPregunta, idOpcionPregunta) VALUES (NULL, $idUsuario, $idPregunta, $opcionSelec)";
+                                        mysqli_query($conexion, $sqlInsert);
+                                    }
+                                    $valorRespuesta = $seleccionesAlumno;
+                                } // if
+                                else{
+                                    $valorRespuesta = $_POST[$nombreInput];
+                                    if($tipoInput == 'textarea'){
+                                        // INSERT para textarea 
+                                        $sqlInsert = "INSERT INTO respuestaUsuario (textoRespuesta, idUsuario, idPregunta, idOpcionPregunta) VALUES ('$valorRespuesta', $idUsuarioLogueado, $idPregunta, NULL)";
+                                    } else {
+                                        // INSERT para radio
+                                        $sqlInsert = "INSERT INTO respuestaUsuario (textoRespuesta, idUsuario, idPregunta, idOpcionPregunta) VALUES (NULL, $idUsuarioLogueado, $idPregunta, $valorRespuesta)";
+                                    }
+                                    mysqli_query($conexion, $sqlInsert);
+                                }//else
+                            }
+                            else{
+                                $consultaRes = "SELECT textoRespuesta, idOpcionPregunta FROM respuestaUsuario WHERE idPregunta = $idPregunta AND idUsuario = $idUsuario";
+                                //$valorRespuesta ="";
+                                $respues= mysqli_query($conexion, $consultaRes);
+                            }
+                        }else{
+                            //Solo se consulta de la base de datos
+                            $consulta
+                        }
+                        //Imprime pues todo pregunta y opciones
+                        echo "<p>$textPreguntas: $valorRespuesta</p>";
+                    }//for
+                if(isset($_POST['enviar-form'])){
+                        $updateEstado = "UPDATE formulario SET enviado = 1 WHERE idFormulario=$id_formulario";
+                        mysqli_query($conexion, $updateEstado);
+                    }
+                }
+                else{
+                    echo "<p>El formulario no ha sido contestado<p>";
+                }
+                ?>
             </div>
             <div class="inferior-derecho">
-                <a href="FormularioCondiciones.php?id_formulario=1">
+                <a href="FormularioCondiciones.php?id_formulario=1"> <!--Modificar la url para que lleve en específico a esa, si no lo enviará a otra página-->
                     <button id="formulario-condiciones">Formulario Condiciones de Estudio</button>
                 </a>
                 <p>Notas de tu profesor: </p>
