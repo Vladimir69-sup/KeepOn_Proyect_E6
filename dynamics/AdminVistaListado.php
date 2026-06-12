6d75c1cdb88f331cb3c692b18d8daf6c00f96788
Vladimir Ortiz Ochoa <vladimirortizochoa62@gmail.com>
2026-06-11 21:14:08 -0600

    fix(listado): se añade el despliegue de los grupos, profesores y alumnos correspondientes

--- dynamics/AdminVistaListado.php
+++ dynamics/AdminVistaListado.php
@@ -1,12 +1,12 @@
 <?php
     include "conexion.php";
 
-    $lista_alumnos = array();
+    $lista_grupos = array();
 
-    $sql = "SELECT nombre, primer_apellido, segundo_apellido FROM infogeneralusuario";
+    $sql = "SELECT * FROM grupo";
     $resultao_query = mysqli_query(connect(), $sql);
-    while($fila = mysqli_fetch_assoc($resultao_query)){
-        $lista_alumnos[] = $fila;
+    while($fila_grupos = mysqli_fetch_assoc($resultao_query)){
+        $lista_grupos[] = $fila_grupos;
     }
 
 ?>
@@ -28,90 +28,77 @@
             <div id="boton-verde">
                 <a href=""><h3>Añadir<br>Alumno<br>O<br>Profesor</h3></a>
             </div>
-        <details>
-            <summary>Grupo 1</summary>
-            <div class="desplegable">
-                <div class="cajita-nombres">
-                    <div>
-                        <p>.......</p>
-                    </div>
-                    <div class="opciones-usuarios">
-                        <a href=""><img class="imgs-2" src=".\..\statics\media\img\waste-bin.png" alt="Bote de basura"></a>
-                        <a href=""><img class="imgs-2" src=".\..\statics\media\img\tache.png"></a>
-                    </div> 
-                </div>
-                <div class="cajita-nombres">
-                    <?php
-                        if (count($lista_alumnos) > 0){
-                            foreach ($lista_alumnos as $alumno) {
-                                echo '<p>'
-                                    . $alumno['nombre'] . ' ' 
-                                    . $alumno['primer_apellido'] . ' '
-                                    . $alumno['segundo_apellido']
-                                    . "</p>";
-                                
-                                echo '<div class="opciones-usuarios">';
-                                echo '<a href=""><img class="imgs-2" src=".\..\statics\media\img\waste-bin.png" alt="Bote de basura"></a>';
-                                echo '<a href=""><img class="imgs-2" src=".\..\statics\media\img\tache.png"></a>';
-                                echo '</div>';
-                            }
-                        }
-                    ?>
-                </div>
-                <div class="cajita-nombres">
-                    <p>Alumno 2</p>
-                    <div class="opciones-usuarios">
-                    <a href=""><img class="imgs-2" src=".\..\statics\media\img\waste-bin.png" alt="Bote de basura"></a>
-                    <a href=""><img class="imgs-2" src=".\..\statics\media\img\tache.png"></a>
-                    </div> 
-                </div>
-                <div class="cajita-nombres">
-                    <p>Alumno 3</p>
-                    <div class="opciones-usuarios">
-                    <a href=""><img class="imgs-2" src=".\..\statics\media\img\waste-bin.png" alt="Bote de basura"></a>
-                    <a href=""><img class="imgs-2" src=".\..\statics\media\img\tache.png"></a>
-                    </div> 
-                </div>
-                <div class="cajita-nombres">
-                    <p>Alumno 4</p>
-                    <div class="opciones-usuarios">
-                    <a href=""><img class="imgs-2" src=".\..\statics\media\img\waste-bin.png" alt="Bote de basura"></a>
-                    <a href=""><img class="imgs-2" src=".\..\statics\media\img\tache.png"></a>
-                    </div> 
-                </div>
-                <div class="cajita-nombres">
-                    <p>Alumno 5</p>
-                    <div class="opciones-usuarios">
-                    <a href=""><img class="imgs-2" src=".\..\statics\media\img\waste-bin.png" alt="Bote de basura"></a>
-                    <a href=""><img class="imgs-2" src=".\..\statics\media\img\tache.png"></a>
-                    </div> 
-                </div>
-            </div>
-        </details>
-        <details class="desplegable">
-            <summary>Grupo 2</summary>
-                <p>Alumno 1<br>Alumno 2<br>Alumno 3<br>Alumno 4<br>Alumno 5</p>
-        </details>
-        <details class="desplegable">
-            <summary>Grupo 3</summary>
-                <p>Alumno 1<br>Alumno 2<br>Alumno 3<br>Alumno 4<br>Alumno 5</p>
-        </details>
-        <details class="desplegable">
-            <summary>Grupo 4</summary>
-                <p>Alumno 1<br>Alumno 2<br>Alumno 3<br>Alumno 4<br>Alumno 5</p>
-        </details>
-        <details class="desplegable">
-            <summary>Grupo 5</summary>
-                <p>Alumno 1<br>Alumno 2<br>Alumno 3<br>Alumno 4<br>Alumno 5</p>
-        </details>
-        <details class="desplegable">
-            <summary>Grupo 6</summary>
-                <p>Alumno 1<br>Alumno 2<br>Alumno 3<br>Alumno 4<br>Alumno 5</p>
-        </details>
-        <details class="desplegable">
-            <summary>Grupo 7</summary>
-                <p>Alumno 1<br>Alumno 2<br>Alumno 3<br>Alumno 4<br>Alumno 5</p>
-        </details>
+
+            <?php
+            if(count($lista_grupos) > 0){
+                foreach($lista_grupos as $grupo){
+                    $nombre_grupo = $grupo['nombreGrupo'];
+                    $id_maestro = $grupo['idMaestro'];
+
+                    $sql2 = "SELECT idUsuario FROM infomaestro WHERE idMaestro = $id_maestro";
+                    $resultao_query2 = mysqli_query(connect(), $sql2);
+                    $resultao = mysqli_fetch_assoc($resultao_query2);
+                    $id_usuario_maestro = $resultao['idUsuario'];
+
+                    $sql3 = "SELECT nombre, primerApellido, segundoApellido FROM infogeneralusuario WHERE idUsuario = $id_usuario_maestro";
+                    $resultao_query3 = mysqli_query(connect(), $sql3);
+                    $resultao_nProfesor = mysqli_fetch_assoc($resultao_query3);
+                    $nombre_profesor = $resultao_nProfesor['nombre'];
+                    $apellido1_profesor = $resultao_nProfesor['primerApellido'];
+                    $apellido2_profesor = $resultao_nProfesor['segundoApellido'];
+
+                    echo "<details>";
+                        echo "<summary>" . $nombre_grupo . "</summary>";
+                        echo "<div class='desplegable'>";
+                            echo "<div class='cajita-nombres'>";
+                                echo "<div>";
+                                    echo "<p>$apellido1_profesor $apellido2_profesor $nombre_profesor</p>";
+                                echo "</div>";
+                                echo "<div class='opciones-usuarios'>";
+                                    echo "<a href=''><img class='imgs-2' src='.\..\statics\media\img\waste-bin.png' alt='Bote de basura'></a>";
+                                    echo '<a href=""><img class="imgs-2" src=".\..\statics\media\img\tache.png"></a>';
+                                echo "</div>"; 
+                            echo "</div>";
+                
+        
+                            echo "<div class='cajita-nombres'>";
+                                $id_grupo = $grupo['idGrupo'];
+                                $sql6 = "SELECT COUNT(idAlumno) FROM infoAlumno WHERE idGrupo = $id_grupo";
+                                $resultao_query6 = mysqli_query(connect(), $sql6);
+                                $resultao_count = mysqli_fetch_assoc($resultao_query6);
+                                $count = $resultao_count['COUNT(idAlumno)'];
+
+                                    //for($i=0 ; $i<=$count; $i++){
+                                        
+                                $sql4 = "SELECT idUsuario FROM infoalumno WHERE idGrupo = $id_grupo";
+                                $resultao_query4 = mysqli_query(connect(), $sql4);
+                                /*$resultao_idUsuario = mysqli_fetch_assoc($resultao_query4);
+                                $resultao_idUsuario = mysqli_fetch_assoc($resultao_query4);*/
+                                //$id_usuario_alumno = $resultao_idUsuario['idUsuario'];
+
+                                while($resultao_idUsuario = mysqli_fetch_assoc($resultao_query4)){
+                                    $id_usuario_alumno = $resultao_idUsuario['idUsuario'];
+                                    $sql5 = "SELECT nombre, primerApellido, segundoApellido FROM infogeneralusuario WHERE idUsuario = $id_usuario_alumno";
+                                    $resultao_query5 = mysqli_query(connect(), $sql5);
+                                    while($resultao_nAlumno = mysqli_fetch_assoc($resultao_query5)){
+                                        $nombre_alumno = $resultao_nAlumno['nombre'];
+                                        $apellido1_alumno = $resultao_nAlumno['primerApellido'];
+                                        $apellido2_alumno = $resultao_nAlumno['segundoApellido'];
+                                        echo "<p>$apellido1_alumno $apellido2_alumno $nombre_alumno</p>";
+                                        echo '<div class="opciones-usuarios">';
+                                        echo '<a href=""><img class="imgs-2" src=".\..\statics\media\img\waste-bin.png" alt="Bote de basura"></a>';
+                                        echo '<a href=""><img class="imgs-2" src=".\..\statics\media\img\tache.png"></a>';
+                                        echo '</div>';
+                                    }
+                                }
+                            echo "</div>";     
+                        //}
+                        echo "</div>";
+                        echo "</details>";
+                }       
+            }
+        
+            ?>
     </div>
 </main>
 </body>
