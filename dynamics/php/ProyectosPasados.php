87b4a73051a336853911c2149bf8e7e6f59bd7a9
Evelin Guadalupe Martinez Sevilla <martinezsevillaevelinguadalupe@gmail.com>
2026-06-13 15:56:12 -0600

    feat:Se añadio estructura

---
+++ dynamics/php/ProyectosPasados.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device_width, initial_scaled=1.0">
+    <meta name="author" content="Evelin Guadalupe Martinez Sevilla">
+    <meta name="description" content="Proyectos de generaciones pasadas">
+    <div id="header"></div>
+    <title>Proyectos</title>
+    <link rel="stylesheet" href="../../statics/css/ProyectosPasados.css">
+	</head>
+<body>
+    <h1>Un Baile Inolvidable</h1>
+    <div class="cont">
+        <img src="../../../img/monkey-rock.png">
+        <div class="rectangulo">
+            <p id="tit">¡Que onda coyolover!</p>
+            <p id="text">Estas pensando en darte de baja de la ete?, si es asi detente un momento,
+            aun te falta mas por conocer, no desesperes coyotito pronto haras cosas increibles 
+            como las de nuestros compañeros de generaciones anteriores, como puedesver a continuacion,
+            juegos que te volaran la mente y codigos que te haran soñar con c++. 
+            Recuerda que eres increible, puedes lograr lo que quieras, no dejes esta experiencia a medias 
+            y nunca olvides que si puedes imaginarlo puede programarlo.</p> 
+        </div>
+    </div>
+    <div class="juegos"> 
+        <div class="juego1">
+            <p id="tit1">Meowstellar</p>
+        </div>
+        <div class="juego2">
+            <p id="tit2">Snoopy</p>
+        </div>
+        <div class="juego3">
+            <p id="tit3">Bancarrota</p>
+        </div>
+    </div>
+</body>
+</html>
