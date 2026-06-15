fe59d393f14db7c96628bcac60d5d4e5da94d428
Luis G <luilli359@gmail.com>
2026-06-15 02:37:39 -0600

    refactor: modificacion de codigo para merge

--- dynamics/php/CrearFormulario.php
+++ dynamics/php/CrearFormulario.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crear Formulario</title>
-    <link rel="stylesheet" href="../../statics/css/style.css">
+    <link rel="stylesheet" href="../../statics/css/formularioProfesores.css">
 
 </head>
 
