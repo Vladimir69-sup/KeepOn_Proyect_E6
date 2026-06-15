1115ae1d766de51b3965034936327c051fc8d68d
Evelin Guadalupe Martinez Sevilla <martinezsevillaevelinguadalupe@gmail.com>
2026-06-15 00:47:51 -0600

    feat:se corrigio ruta de imgs de proyectos

--- dynamics/php/ProyectosPasados.php
+++ dynamics/php/ProyectosPasados.php
@@ -12,7 +12,7 @@
 <body>
     <h1>Un Baile Inolvidable</h1>
     <div class="cont">
-        <img src="../../../img/monkey-rock.png">
+        <img src="../../statics/media/img/monkey-rock.png">
         <div class="rectangulo">
             <p id="tit">¡Que onda coyolover!</p>
             <p id="text">Estas pensando en darte de baja de la ete?, si es asi detente un momento,
@@ -26,7 +26,7 @@
     <div class="juegos"> 
         <div class="juego1">
             <p id="tit1">Meowstellar</p>
-            <img src="../../../img/meowstellar1.png" class="imagenes">
+            <img src="../../statics/media/img/meowstellar1.png" class="imagenes">
             <p class="desc">Eres un explorador espacial, tu nave se averió en la luna
                 ¿Podrás superar los desafíos? ¿Lograrás encontrar a tu nave 
                 y a tu tripulación?</p>
@@ -34,14 +34,14 @@
         </div>
         <div class="juego2">
             <p id="tit2">Snoopy</p>
-            <img src="../../../img/snoopy1.png" class="imagenes">
+            <img src="../../statics/media/img/snoopy1.png" class="imagenes">
             <p class="desc">Woodstock está perdido,y Snoopy deberá encontrarlo
                 ¿Podrás superar los desafíos? </p>
             <button class="botones">Mas Informacion</button>
         </div>
         <div class="juego3">
             <p id="tit3">Juan Garnachas</p>
-            <img src="../../../img/bancarrota1.png" class="imagenes">
+            <img src="../../statics/media/img/bancarrota1.png" class="imagenes">
             <p class="desc">Eres dueño de un negocio y tienes que triunfar en las garnachas
                 ¿Lograras pasar cada nivel sin caer en banca rota?
             </p>
@@ -49,7 +49,7 @@
         </div>
         <div class="juego4">
             <p id="tit3">Coyotron</p>
-            <img src="../../../img/coyotron1.png" class="imagenes">
+            <img src="../../statics/media/img/coyotron1.png" class="imagenes">
             <p class="desc">Eres un estudiante de prepa 6, tendras que aprobar
                 las distintas materias comprendidas en los tres grados de bachillerato
                 ¿Lograras graduarte?</p>
@@ -57,7 +57,7 @@
         </div>
         <div class="juego5">
             <p id="tit3">Bebes en peligro</p>
-            <img src="../../../img/bebes1.png" class="imagenes">
+            <img src="../../statics/media/img/bebes1.png" class="imagenes">
             <p class="desc">Eres una cigueña y tendras que llevar a salvo a un bebe con 
                 sus nuevos papas ¿Seras capaz de esquivar los obstaculos?
             </p>
