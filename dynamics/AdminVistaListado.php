a90fe89fc0dbccf43398bec1ac02a13a08927844
Vladimir Ortiz Ochoa <vladimirortizochoa62@gmail.com>
2026-06-14 19:41:42 -0600

    fix(listado): se realizan correcciones a la muestra y funciones del listado

--- dynamics/AdminVistaListado.php
+++ dynamics/AdminVistaListado.php
@@ -26,7 +26,7 @@
     <div id="caja-principal-2">
         <h1>Alumnos y profesorado</h1>
             <div id="boton-verde">
-                <a href=""><h3>Añadir<br>Alumno<br>O<br>Profesor</h3></a>
+                <a href=".\AdminVistaAdicion.php"><h3>Añadir<br>Alumno<br>O<br>Profesor</h3></a>
             </div>
 
             <?php
@@ -52,16 +52,10 @@
                         echo "<div class='desplegable'>";
                             echo "<div class='cajita-nombres'>";
                                 echo "<div>";
-                                    echo "<p>$apellido1_profesor $apellido2_profesor $nombre_profesor</p>";
-                                echo "</div>";
-                                echo "<div class='opciones-usuarios'>";
-                                    echo "<a href=''><img class='imgs-2' src='.\..\statics\media\img\waste-bin.png' alt='Bote de basura'></a>";
-                                    echo '<a href=""><img class="imgs-2" src=".\..\statics\media\img\tache.png"></a>';
+                                    echo "<p>Profesor: $apellido1_profesor $apellido2_profesor $nombre_profesor</p><br>";
                                 echo "</div>"; 
                             echo "</div>";
-                
-        
-                            echo "<div class='cajita-nombres'>";
+
                                 $id_grupo = $grupo['idGrupo'];
                                 $sql6 = "SELECT COUNT(idAlumno) FROM infoAlumno WHERE idGrupo = $id_grupo";
                                 $resultao_query6 = mysqli_query(connect(), $sql6);
@@ -81,17 +75,35 @@
                                     $sql5 = "SELECT nombre, primerApellido, segundoApellido FROM infogeneralusuario WHERE idUsuario = $id_usuario_alumno";
                                     $resultao_query5 = mysqli_query(connect(), $sql5);
                                     while($resultao_nAlumno = mysqli_fetch_assoc($resultao_query5)){
-                                        $nombre_alumno = $resultao_nAlumno['nombre'];
-                                        $apellido1_alumno = $resultao_nAlumno['primerApellido'];
-                                        $apellido2_alumno = $resultao_nAlumno['segundoApellido'];
-                                        echo "<p>$apellido1_alumno $apellido2_alumno $nombre_alumno</p>";
-                                        echo '<div class="opciones-usuarios">';
-                                        echo '<a href=""><img class="imgs-2" src=".\..\statics\media\img\waste-bin.png" alt="Bote de basura"></a>';
-                                        echo '<a href=""><img class="imgs-2" src=".\..\statics\media\img\tache.png"></a>';
-                                        echo '</div>';
+                                        echo "<div class='cajita-nombres'>";
+                                            $nombre_alumno = $resultao_nAlumno['nombre'];
+                                            $apellido1_alumno = $resultao_nAlumno['primerApellido'];
+                                            $apellido2_alumno = $resultao_nAlumno['segundoApellido'];
+                                            echo "<p>$apellido1_alumno $apellido2_alumno $nombre_alumno</p>";
+                                            echo '<div class="opciones-usuarios">';
+                                            echo "<input type = 'checkbox' id= 'ops_alumno'>";
+                                            echo "<label for = 'ops_alumno'>";
+                                                echo '<img class="imgs-2" src=".\..\statics\media\img\waste-bin.png" alt="Bote de basura">';
+                                            echo "</label>";
+                                                echo "<section id = 'mensaje'>";
+                                                    echo "<h3>¿Está seguro de querer eliminar a $apellido1_alumno $apellido2_alumno $nombre_alumno?";
+                                                    echo "<div id = 'boton_si'>";
+                                                        echo "<form action = './eliminacionUsuario.php' method='POST'>";
+                                                            echo "<input type = 'hidden' id = 'envio_idAlumno' name = 'envio_idAlumno' value='$id_usuario_alumno'>";
+                                                            echo "<input type = 'submit' id= 'verif'>";
+                                                            echo "<label for = 'verif'>";
+                                                                echo "<p id = 'boton_yes'>Estoy seguro</p>";
+                                                            echo "</label>";
+                                                        echo "</form>";
+                                                    echo "</div>";
+                                                    echo "<div id = 'boton_no'>";
+                                                        echo "<p>Vuelva a dar click en el botón para cerrar la ventana</p>";
+                                                    echo "</div>";
+                                                echo "</section>";
+                                            echo '</div>';
+                                        echo "</div>";  
                                     }
                                 }
-                            echo "</div>";     
                         //}
                         echo "</div>";
                         echo "</details>";
