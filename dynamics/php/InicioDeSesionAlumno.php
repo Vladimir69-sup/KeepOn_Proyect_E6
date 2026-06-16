f7a59120c9fe4b376b9b4a6534193f3f5f700486
Luana Alvarez <luana.alvarez@ciencias.unam.mx>
2026-06-16 06:37:55 -0600

    feat: Corrige las redirecciones al momento de iniciar sesion como alumno y refactoriza la página de inicio del alumno de acuerdo al alcance del proyecto.

--- dynamics/php/InicioDeSesionAlumno.php
+++ dynamics/php/InicioDeSesionAlumno.php
@@ -1,5 +1,6 @@
 <?php 
-    include 'Conexion.php'; //Trae archivo que conecta todo a base de datos 
+
+    include 'conexion.php'; //Trae archivo que conecta todo a base de datos 
     include 'Validaciones.php'; //Trae archivo que Valida y Limpia los datos
     session_start(); //indica al servidor la peticion que estoy haciendo  y que este guarde los datos
 
@@ -16,37 +17,43 @@
                             $error = "Verifica que tu usuario y/o contraseña sean correctos"; // marca la variable error que resulta en ese mensaje
                             
                         }
-                    if(!filter_var($usuario_limpio, FILTER_VALIDATE_INT)) // este es como un filtro que valida que dentro del usuario no se escriban letras si no solamente numeros 
+                    elseif(!filter_var($usuario_limpio, FILTER_VALIDATE_INT)) // este es como un filtro que valida que dentro del usuario no se escriban letras si no solamente numeros 
                         {
                             $error = "El usuario debe ser numérico"; //marca variable error que resulta en el mensaje 
                             
                         }
-                    //Tengo que cambiar solo esta linea para los demas inicios de sesions
-                    $identificador = "SELECT * FROM infoAlumno WHERE numeroCuenta = $usuario_limpio"; //regresa todas las columnas de informacion donde el numero de cuenta es igual al que nos paso el usuario
-                    $consulta1 = mysqli_query (connect(), $identificador); //ejecuta la consulta que esta en el renglon de arriba 
-                    if(mysqli_num_rows($consulta1) === 1) //verifica que el objeto iterable solo nos regrese una fila
-                        {
-                            $res = mysqli_fetch_assoc($consulta1); // transforma la fila en un arreglo asociativo
-                            $idUsuario = $res["idUsuario"]; //guarda el unico id del usuario
-                            $consulta2 = mysqli_query (connect(), "SELECT (fechaNacimiento) FROM infogeneralusuario WHERE idUsuario = $idUsuario"); //
-                            $res2 = mysqli_fetch_assoc($consulta2); //transforma la fila en un arreglo asociativo
-                            $contrasenadb = $res2["fechaNacimiento"]; //guarda la fecha de nacimiento del usuario
-                            if($contrasenadb === $contrasena_limpia) // compara si la contraseña que escribio el usuario es igual a la que esta en base de datos
-                                {
-                                    $_SESSION["idAlumno"] = $res["idAlumno"]; //Guarda el ID
-                                    $_SESSION["idGrupo"] = $res["idGrupo"]; //Guarda el ID
-                                    $_SESSION["idUsuario"] = $res["idUsuario"]; //Guarda el ID
-                                    header ("Location: ./inicio.php");
-                                    
-                                }
-                            else // da otra opcion por si lo de arriba no llegara a pasar
-                                {
-                                    $error ="Contraseña Incorrecta"; // marca la variable error con el mensaje que se escribio
-                                    
+                    else{                    
+                        //Tengo que cambiar solo esta linea para los demas inicios de sesions
+                        $identificador = "SELECT * FROM infoAlumno WHERE numeroCuenta = $usuario_limpio"; //regresa todas las columnas de informacion donde el numero de cuenta es igual al que nos paso el usuario
+                        $consulta1 = mysqli_query (connect(), $identificador); //ejecuta la consulta que esta en el renglon de arriba 
+                        if($consulta1 && mysqli_num_rows($consulta1) === 1) //verifica que el objeto iterable solo nos regrese una fila
+                            {
+                                $res = mysqli_fetch_assoc($consulta1); // transforma la fila en un arreglo asociativo
+                                $idUsuario = $res["idUsuario"]; //guarda el unico id del usuario
+                                $consulta2 = mysqli_query (connect(), "SELECT (fechaNacimiento) FROM infoGeneralUsuario WHERE idUsuario = $idUsuario"); //
+                                $res2 = mysqli_fetch_assoc($consulta2); //transforma la fila en un arreglo asociativo
+                                $contrasenadb = $res2["fechaNacimiento"]; //guarda la fecha de nacimiento del usuario
+                                if($contrasenadb === $contrasena_limpia) // compara si la contraseña que escribio el usuario es igual a la que esta en base de datos
+                                    {
+                                        $_SESSION["idAlumno"] = $res["idAlumno"]; //Guarda el ID
+                                        $_SESSION["idGrupo"] = $res["idGrupo"]; //Guarda el ID
+                                        $_SESSION["idUsuario"] = $res["idUsuario"]; //Guarda el ID
+                                        header ("Location: ./alumno.php"); 
+                                        exit();  
+                                        
+                                    }
+                                else // da otra opcion por si lo de arriba no llegara a pasar
+                                    {
+                                        $error ="Contraseña Incorrecta"; // marca la variable error con el mensaje que se escribio
+                                        
 
-                                }
+                                    }
 
-                        }         
+                            }
+                            else{
+                                $error="El usuario no está registrado.";
+                            }      
+                    }   
                 }
                 else // da otra opcion por si lo de arriba no llegara a pasar
                     {
